85e96521b762358efd5d41234f1adc7e49af3cbe
Ruslan <esprgv@gmail.com>
2024-05-04 19:24:01 +0400

    server: Маршрут для тестов, докеризация

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,12 @@
 // Собираем все маршруты
-import express from 'express';
+import express, { Request, Response } from 'express';
 import authRoutes from './authRoutes';
 import imageRoutes from './imageRoutes';
 
 const router = express.Router();
 
 // Привязываем маршруты из разных файлов
+router.get('/test', (_, res: Response,) => { res.json({ test: "Маршрут для теста, всё работает👍" }) });
 router.use('/auth', authRoutes);
 router.use('/images', imageRoutes);
 
